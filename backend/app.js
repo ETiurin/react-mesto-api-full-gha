606c5df1d7e65b21b4ccffc7b8aa69427ca6be72
ETiurin <117716801+ETiurin@users.noreply.github.com>
2023-08-18 00:26:12 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,12 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
 const rateLimit = require('express-rate-limit');
+const allowedCors = require('cors');
 const { loginValidation, createUserValidation } = require('./middlewares/validations');
 const auth = require('./middlewares/auth');
 const NotFoundError = require('./errors/NotFoundError');
 const { login, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
-const allowedCors = require('./middlewares/allowedCors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
